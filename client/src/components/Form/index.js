c8f50f1516ed8cfc418199b12587d30376f8011d
Anjali Sheth <anjali.sheth@otis.com>
2021-04-29 16:02:37 -0400

    navbar and header componements updated

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 
-
 function Form(props){
 return (
   <div className="container">
   <form>
     <div className="form-group">
         {/* <label htmlFor="search"><h2>Search for Books of Interest and Save </h2></label> */}
-        <h1 style={{ fontSize: "1.5em", color: "MediumSeaGreen" }}>
-               <strong >Search </strong>  and  <strong> Save Recipes</strong>  
+        <h1 style={{ fontSize: "1.5em", fontweigh: "bold", color: "white"}}>
+               < >Search </>  and  <> Save Recipes</>  
             </h1><br></br>
       <input
         onChange={props.handleInputChange}
