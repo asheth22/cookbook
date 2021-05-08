c56a3c1d237435bcea195c76572148b5a75d0f9a
ADecastro <a.decastro1108@gmail.com>
2021-05-08 13:13:59 -0400

    Trimmed irrellevant comments and console logs from controllers and models

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,6 @@ const db = require("../models");
 // Defining methods for the userController
 module.exports = {
   getUser: (req, res, next) => {
-    console.log('===== user!!======');
-    console.log(req.user);
     if (req.user) {
       return res.json({ user: req.user });
     } else {
@@ -13,7 +11,6 @@ module.exports = {
   },
   
   register: function (req, res) {
-    console.log("inside create: ", req.body)
     db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
@@ -24,25 +21,20 @@ module.exports = {
   },
   logout: (req, res) => {
     if (req.user) {
-      console.log("Logout");
       req.session.destroy();
-      res.clearCookie('connect.sid'); // clean up!
+      res.clearCookie('connect.sid'); 
       return res.json({ msg: 'logging you out' });
     } else {
       return res.json({ msg: 'no user to log out!' });
     }
   },
   auth: function(req, res, next) {
-		console.log(req.body);
-		console.log('================');
 		next();
   },
   authenticate: (req, res) => {
-		console.log('POST to /login');
-		const user = JSON.parse(JSON.stringify(req.user)); // hack
+		const user = JSON.parse(JSON.stringify(req.user));
 		const cleanUser = Object.assign({}, user);
 		if (cleanUser) {
-			console.log(`Deleting ${cleanUser.password}`);
 			delete cleanUser.password;
 		}
 		res.json({ user: cleanUser });
